a0cec000b12828b47a7a277c1e379932f13a8650
João Vitor <joaovitorfelixcontato@gmail.com>
2026-03-06 17:04:57 -0300

    refactor(server): update TokenIssuerAdapter to use AccessTokenPayload for claims

--- internal/adapter/auth/token_issuer_adapter.go
+++ internal/adapter/auth/token_issuer_adapter.go
@@ -1,11 +1,11 @@
 package auth
 
 import (
-	"encoding/json"
 	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/joao-vitor-felix/cinemax/internal/core/port"
 )
 
 type TokenIssuerAdapter[C any] struct {
@@ -16,31 +16,23 @@ func NewTokenIssuerAdapter[C any](secret string) *TokenIssuerAdapter[C] {
 	return &TokenIssuerAdapter[C]{secret: secret}
 }
 
-func (ti *TokenIssuerAdapter[C]) Generate(claims C, expiresIn time.Duration) (string, error) {
-	data, err := json.Marshal(claims)
-	if err != nil {
-		return "", fmt.Errorf("token_issuer_adapter: failed to marshal claims: %w", err)
-	}
-
+func (ti *TokenIssuerAdapter[C]) Generate(claims port.AccessTokenPayload) (string, error) {
 	mapClaims := jwt.MapClaims{
-		"exp": time.Now().Add(expiresIn).Unix(),
-		"iat": time.Now().Unix(),
+		"exp":   time.Now().Add(time.Hour).Unix(),
+		"iat":   time.Now().Unix(),
+		"sub":   claims.Id,
+		"email": claims.Email,
+		"role":  claims.Role,
 	}
-	if err := json.Unmarshal(data, &mapClaims); err != nil {
-		return "", fmt.Errorf("token_issuer_adapter: failed to build map claims: %w", err)
-	}
-
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, mapClaims)
 	signed, err := token.SignedString([]byte(ti.secret))
 	if err != nil {
 		return "", fmt.Errorf("token_issuer_adapter: failed to sign token: %w", err)
 	}
-
 	return signed, nil
 }
 
-func (ti *TokenIssuerAdapter[C]) Validate(tokenStr string) (C, error) {
-	payload := *new(C)
+func (ti *TokenIssuerAdapter[C]) Validate(tokenStr string) (*port.AccessTokenPayload, error) {
 	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("token_issuer_adapter: unexpected signing method: %v", token.Header["alg"])
@@ -49,17 +41,18 @@ func (ti *TokenIssuerAdapter[C]) Validate(tokenStr string) (C, error) {
 	})
 
 	if err != nil || !token.Valid {
-		return payload, fmt.Errorf("token_issuer_adapter: failed to parse token: %w", err)
+		return &port.AccessTokenPayload{}, fmt.Errorf("token_issuer_adapter: failed to parse token: %w", err)
 	}
 
-	data, err := json.Marshal(token.Claims)
-	if err != nil {
-		return payload, fmt.Errorf("token_issuer_adapter: failed to marshal map claims: %w", err)
+	payload, ok := token.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return &port.AccessTokenPayload{}, fmt.Errorf("token_issuer_adapter: failed to cast claims to map claims")
 	}
 
-	if err := json.Unmarshal(data, &payload); err != nil {
-		return payload, fmt.Errorf("token_issuer_adapter: failed to unmarshal claims: %w", err)
-	}
-
-	return payload, nil
+	return &port.AccessTokenPayload{
+		Id:    payload["sub"].(string),
+		Email: payload["email"].(string),
+		Role:  payload["role"].(string),
+	}, nil
 }
--- internal/core/port/token_issuer.go
+++ internal/core/port/token_issuer.go
@@ -6,3 +6,9 @@ type TokenIssuer[C any] interface {
 	Generate(claims C, expiresIn time.Duration) (string, error)
 	Validate(token string) (C, error)
 }
+
+type AccessTokenPayload struct {
+	Id    string
+	Email string
+	Role  string
+}
