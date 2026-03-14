3a97855013b7f253fd08063f21648acbc4df327c
João Vitor <joaovitorfelixcontato@gmail.com>
2026-03-14 18:53:15 -0300

    refactor(server): remove factory; add controller builds to container

--- internal/factory/container.go
+++ internal/factory/container.go
@@ -3,15 +3,35 @@ package factory
 import (
 	"database/sql"
 
+	"github.com/joao-vitor-felix/cinemax/internal/adapter/auth"
 	"github.com/joao-vitor-felix/cinemax/internal/adapter/controller"
+	"github.com/joao-vitor-felix/cinemax/internal/adapter/repository"
+	"github.com/joao-vitor-felix/cinemax/internal/core/service"
 )
 
 type Container struct {
-	UserController *controller.UserController
+	UserController         *controller.UserController
+	SignInController       *controller.SignInController
+	RefreshTokenController *controller.RefreshTokenController
 }
 
 func NewContainer(db *sql.DB) *Container {
+	// Adapters
+	passwordHasher := auth.NewPasswordHasherAdapter()
+	tokenIssuer := auth.NewTokenIssuerAdapter()
+
+	// Repositories
+	userRepo := repository.NewPostgresUserRepository(db)
+	refreshTokenRepo := repository.NewPostgresRefreshTokenRepository(db)
+
+	// Services
+	userService := service.NewUserService(userRepo, passwordHasher)
+	signInService := service.NewSignInService(userRepo, passwordHasher, tokenIssuer, refreshTokenRepo)
+	refreshTokenService := service.NewRefreshTokenService(refreshTokenRepo, userRepo, tokenIssuer)
+
 	return &Container{
-		UserController: MakeNewUserController(db),
+		UserController:         controller.NewUserController(userService),
+		SignInController:       controller.NewSignInController(signInService),
+		RefreshTokenController: controller.NewRefreshTokenController(refreshTokenService),
 	}
 }
