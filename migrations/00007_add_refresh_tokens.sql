db4a8a4fa0e19bf035f285cab4310d0d0cf4d156
João Vitor <joaovitorfelixcontato@gmail.com>
2026-03-14 18:51:55 -0300

    feat(server): change token type to UUID in refresh_tokens table

--- migrations/00007_add_refresh_tokens.sql
+++ migrations/00007_add_refresh_tokens.sql
@@ -1,13 +1,11 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS refresh_tokens (
-	token VARCHAR(255) PRIMARY KEY,
+	token UUID PRIMARY KEY,
 	user_id UUID REFERENCES users,
 	expires_at TIMESTAMPTZ NOT NULL,
 	used_at TIMESTAMPTZ,
 	created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-COMMENT ON COLUMN refresh_tokens.token IS 'A SHA-256 hash of token';
 
 -- +goose Down
 DROP TABLE IF EXISTS refresh_tokens;
