d5725860e7b770c9cdcf8b7b406ef8f0b79968ea
João Vitor <joaovitorfelixcontato@gmail.com>
2026-04-04 16:20:27 -0300

    refactor(server): remove unused SignInRepository interface from sign_in.go

--- internal/core/port/sign_in.go
+++ internal/core/port/sign_in.go
@@ -1,11 +1,5 @@
 package port
 
-import "github.com/joao-vitor-felix/cinemax/internal/core/domain"
-
-type SignInRepository interface {
-	FindByEmail(email string) (*domain.User, error)
-}
-
 type SignInInput struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=8,max=12"`
